2aa8e00cbb45f8fcf919be4b33707744d5980aa9
Robert McCurdy <freeload101@users.noreply.github.com>
2023-10-23 11:59:16 -0400

    Update AZADBASGER.py

--- AZADBASGER.py
+++ AZADBASGER.py
@@ -1,162 +1,58 @@
+"""
+
+"""
+
+
 import phantom.rules as phantom
 import json
 from datetime import datetime, timedelta
 
-phantom.debug("input_filter() called")
-
-    
-################################################################################
-## Global Custom Code Start
-################################################################################
-
-################################################################################
-## Global Custom Code End
-################################################################################
 
 @phantom.playbook_block()
 def on_start(container):
     phantom.debug('on_start() called')
 
-    # call 'code_1' block
-    code_1(container=container)
-    import logging
-    import sys
-    sys.stderr.write('Error message\n')
-    #logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
-    logging.debug('This message should go to the log file')
-    logging.info('So should this')
-    logging.warning('And this, too')
-    logging.error('And non-ASCII stuff, too, like Øresund and Malmö')
+    # call 'debug_1' block
+    debug_1(container=container)
+
     return
 
 @phantom.playbook_block()
-def code_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
-    phantom.debug("code_1() called")
+def debug_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, **kwargs):
+    phantom.debug("debug_1() called")
+
+    container_artifact_data = phantom.collect2(container=container, datapath=["artifact:*.data.userId","artifact:*.data.requestId","artifact:*.requestId","artifact:*.id","artifact:*.external_id"])
+
+    container_artifact_header_item_0 = [item[0] for item in container_artifact_data]
+    container_artifact_header_item_1 = [item[1] for item in container_artifact_data]
+    container_artifact_header_item_2 = [item[2] for item in container_artifact_data]
+
+    parameters = []
+
+    parameters.append({
+        "input_1": container_artifact_header_item_0,
+        "input_2": container_artifact_header_item_1,
+        "input_3": container_artifact_header_item_2,
+        "input_4": None,
+        "input_5": None,
+        "input_6": None,
+        "input_7": None,
+        "input_8": None,
+        "input_9": None,
+        "input_10": None,
+    })
 
     ################################################################################
     ## Custom Code Start
     ################################################################################
-    import json
-    import requests
-    import time
-    import array
-    import yaml
-    
-    # for async
-    import asyncio
-    import logging
-    
-    # for command line args
-    import sys
-    
-    # get the byte size of a vars
-    from sys import getsizeof
-    
-    
-    # for file mamagment backup of JSON output etc..
-    import shutil
-    
-    # for skipping cert errors in proxy
-    import ssl
-    # hiding warnings for ssl
-    import urllib3
-    
-    # for setting OS env vars for proxy
-    import os
-    
-    # for checking for files existing
-    import pathlib
-    
-    # This is set to False when we enable proxy support later
-    VAR_SSL_FLAG=True
-    
-    
-    # regex
-    import re
-    
-    # to check dates on last seen key
-    import datetime
-    #import dateutil.parser
-    import pytz
-    
-    ############################################ DANGER SECURTY ############
-    ############################################ DANGER SECURTY ############
-    ############################################ DANGER SECURTY ############
-    ############################################ DANGER SECURTY ############
-    
-    with open("config.yml", "w") as file:
-       lines = ['{\r        "app_id":"66f2XXXXXXXXXXXXXXXXXXXXXXXXXX496f98",\r        "app_id_secret":"gKK8QXXXXXXXXXXXXXXXXXXXXXXXXXXc_a",\r        "tenant_id":"6663XXXXXXXXXXXXXXXXXXXXXXXXXX2154"\r}']
-       file.writelines(lines)
-       file.close()
-    
-    ############################################ DANGER SECURTY ############
-    ############################################ DANGER SECURTY ############
-    ############################################ DANGER SECURTY ############
-    ############################################ DANGER SECURTY ############
-    
-    
-    
-    
-    
-    ############################################################ FUNCTIONS
-    def LOADCONFIG():
-       print("DEBUG: Running LOADCONFIG")
-       yaml.warnings({'YAMLLoadWarning': False})
-    
-       Loader = yaml.FullLoader
-       with open("config.yml", 'r') as ymlfile:
-          cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
-       global app_id
-       global app_id_secret
-       global tenant_id
-       app_id = (cfg['app_id'])
-       app_id_secret = (cfg['app_id_secret'])
-       tenant_id = (cfg['tenant_id'])
-    
-    
-    def PROXY():
-       print("DEBUG: Running PROXY")
-       os.environ["HTTP_PROXY"] = "http://127.0.0.1:8080"
-       os.environ["HTTPS_PROXY"] = "http://127.0.0.1:8080"
-       global VAR_SSL_FLAG
-       VAR_SSL_FLAG = False
-       # supress warnings for ssl
-       urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-    
-    ################################################################################ AUTH
-    def FUNC_AUTH():
-       print("DEBUG: Running FUNC_AUTH")
-       r = requests.post('https://login.microsoftonline.com/' + tenant_id + '/oauth2/v2.0/token',
-                         data={'grant_type': 'client_credentials', 'client_id': app_id, 'client_secret': app_id_secret,
-                               'scope': 'https://graph.microsoft.com/.default'}, verify=VAR_SSL_FLAG)
-       if r.status_code in [200]:
-          # print("DEBUG: Status 200 OK!")
-          tok_dict = json.loads(r.text)
-          global access_token
-          access_token = tok_dict["access_token"]
-          print("DEBUG: Access Token is ", access_token)
-       else:
-          print(r.status_code)
-          print(r.url)
-          print(r.text)
-          print(r.content)
-          print("ERROR DID NOT GET 201 STATUS CODE")
-    
-       ############################################################ MAIN
-    
-    #LOADCONFIG()
-    #PROXY()
-    #FUNC_AUTH()
-    sys.stderr.write('Error message\n')
-    #logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
-    logging.debug('This message should go to the log file')
-    logging.info('So should this')
-    logging.warning('And this, too')
-    logging.error('And non-ASCII stuff, too, like Øresund and Malmö')
+
+    # Write your custom code here...
 
     ################################################################################
     ## Custom Code End
     ################################################################################
+
+    phantom.custom_function(custom_function="community/debug", parameters=parameters, name="debug_1")
 
     return
 
@@ -168,15 +64,8 @@ def on_finish(container, summary):
     ################################################################################
     ## Custom Code Start
     ################################################################################
-    import logging
-    import sys
+
     # Write your custom code here...
-    sys.stderr.write('Error message\n')
-    #logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
-    logging.debug('This message should go to the log file')
-    logging.info('So should this')
-    logging.warning('And this, too')
-    logging.error('And non-ASCII stuff, too, like Øresund and Malmö')
 
     ################################################################################
     ## Custom Code End
