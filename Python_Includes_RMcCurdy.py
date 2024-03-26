b6f842784318b0d504b23d95cd13303f00759ac6
Robert McCurdy <freeload101@users.noreply.github.com>
2024-03-26 17:29:02 -0400

    functions for making APIs from scratch

--- Python_Includes_RMcCurdy.py
+++ Python_Includes_RMcCurdy.py
@@ -1,151 +1,198 @@
-import logging
-logging.basicConfig(level=logging.DEBUG)
+import json
 import requests
-import pprint
-import re
-
-
-# FOR PROXY STUFF
+import time
+import array
+import yaml
+# for async
+import asyncio
+# for logging
+import logging
 # for command line args
 import sys
+# get the byte size of a vars
+from sys import getsizeof
+# for file management backup of JSON output etc.
+import shutil
 # for skipping cert errors in proxy
 import ssl
 # hiding warnings for ssl
 import urllib3
 # for setting OS env vars for proxy
 import os
-VAR_SSL_FLAG = True
-# END PROXY STUFF
-
+# for checking for files existing
+import pathlib
+# regex
+import re
+import datetime
+import logging
+import requests
+import pprint
+import re
 # hidden inputs FTW __VIEWSTATE etc ...!
 from bs4 import BeautifulSoup
 
-
-###################################################### FUNCs  ..
-
-################################################################################ AUTH
-def FUNC_AUTH():
-    phantom.debug("DEBUG: Running FUNC_AUTH")
-    r = requests.post('https://login.microsoftonline.com/' + tenant_id + '/oauth2/v2.0/token',
-                      data={'grant_type': 'client_credentials', 'client_id': app_id, 'client_secret': app_id_secret, 'scope': 'https://graph.microsoft.com/.default'}, verify=VAR_SSL_FLAG)
-    if r.status_code in [200]:
-        tok_dict = json.loads(r.text)
-        global access_token
-        access_token = tok_dict["access_token"]
-    else:
-        phantom.error(r.status_code)
-        phantom.error(r.url)
-        phantom.error(r.text)
-        phantom.error(r.content)
-        phantom.error("ERROR DID NOT GET 201 STATUS CODE")
-
-    # headers = {'Authorization': }
-    global headers
-    headers = {"Authorization": f"Bearer {access_token}", "Content-Type": "application/json"}
-    r = requests.post('https://graph.microsoft.com/v1.0/IdentityProtection/riskyUsers/dismiss', data=json.dumps(
-        {'userIds': [data_summary_userid_value]}),
-                      headers=headers, verify=VAR_SSL_FLAG)
-
-    #json_data = json.dumps(data)
-    if r.status_code in [204]:
-        phantom.debug("DEBUG: Status 204 OK ")
-        phantom.debug(r.status_code)
-        phantom.debug(r.url)
-        phantom.debug(r.text)
-        phantom.debug(r.content)
-    else:
-        phantom.error(r.status_code)
-        phantom.error(r.url)
-        phantom.error(r.text)
-        phantom.error(r.content)
-        phantom.error("ERROR DID NOT GET 201 STATUS CODE")
-
-
-# use YAML for creds no plain text creds in scripts !
-def LOADCONFIG():
-    phantom.debug("DEBUG: Running LOADCONFIG")
-    yaml.warnings({'YAMLLoadWarning': False})
-
-    Loader = yaml.FullLoader
-    with open("config.yml", 'r') as ymlfile:
-        cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
-    global app_id
-    global app_id_secret
-    global tenant_id
-    app_id = (cfg['app_id'])
-    app_id_secret = (cfg['app_id_secret'])
-    tenant_id = (cfg['tenant_id'])
-
-
-
-# for Splunk SOAR logging ... 
-class phantom:
-    @staticmethod
-    def error(message):
-        logging.error(f"Error: {message}")
-
-    @staticmethod
-    def debug(message):
-        logging.debug(f"Debug: {message}")
-
-
-# Proxy support see BurpSuite!!!
-def PROXY():
-    print("DEBUG: Running PROXY")
-    os.environ["HTTP_PROXY"] = "http://127.0.0.1:8080"
-    os.environ["HTTPS_PROXY"] = "http://127.0.0.1:8080"
-    global VAR_SSL_FLAG
-    VAR_SSL_FLAG = False
-    # supress warnings for ssl
-    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
-# sso.connect.pingidentity.com
-# sso.newellrubbermaid.com
+"""
+# * this python script is my includes and functions for debugging and writing APIs
+#  BEFORE Phantom COPY/PASTE !!!
+# * REPLACE TABS WITH SPACES
+# * REMOVE PHANTOM.LOGG CLASS
+# * DISABLE PROXY
 
 # print out stuff and it look human readable ....
 def get_var_name(var, globals=globals()):
+    Phantom.debug("DEBUG: Running get_var_name")
     return [name for name, val in globals.items() if val is var]
+"""
 
-def pprint_ForRealz(varname,varvalue):
-    # replace common word boundaries
-    varvalue = re.sub(r'\s+', '_', varvalue)
-    varvalue = re.sub(r'<', '\n', varvalue)
-    #varvalue = re.sub(r'=', '\=', varvalue)
-    # replace all left over control chars with \n ... if you want ...
-    #var1 = re.sub(r'\b', '\n', var1)
-    logging.info("\n================\n"+ varname +": " + varvalue +"\n================\n")
+# for showing debug / error locally
+logging.basicConfig(level=logging.DEBUG)
+
+"""
+FUNCTIONS/CLASSES
+"""
 
 
-def FindCookie(HTMLContent,CookieSearch):
+# Used for Splunk Phantom because you can't use logging.error or logging.debug to get any output.
+class Phantom:
+    @staticmethod
+    def error(message):
+        logging.error(f"*** {message}  ***")
+
+    @staticmethod
+    def debug(message):
+        logging.debug(f"+++ {message}")
+
+
+def load_config(file_path):
+    Phantom.debug("load_config")
+    with open(file_path, 'r') as file:
+        return json.load(file)
+
+
+def proxy_enable():
+    Phantom.debug("proxy_enable")
+    os.environ["HTTP_PROXY"] = "http://127.0.0.1:8080"
+    os.environ["HTTPS_PROXY"] = "http://127.0.0.1:8080"
+    # supress warnings for ssl
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+def pprint_simp(varname, pprint_value):
+    """
+    The point of this function is to basically parse anything and make it human-readable
+    """
+    Phantom.debug("pprint_simp")
+    Phantom.debug("Type:  " + str(type(pprint_value)))
+    try:
+        # json.loads(pprint_value)
+        print(json.dumps(pprint_value, indent=4))
+    except ValueError:
+        # convert it to string
+        pprint_value = str(pprint_value)
+        # replace common word boundaries
+        pprint_value = re.sub(r'\s+', '_', pprint_value)
+        pprint_value = re.sub(r'<', '\n', pprint_value)
+        pprint_value = re.sub(r'=', '\n=', pprint_value)
+        # replace all leftover control chars with \n ... if you want ...
+        # pprint_value = re.sub(r'\b', '\n', pprint_value)
+        Phantom.debug("\n================\n" + varname + ": " + pprint_value + "\n================\n")
+
+
+def find_cookie(html_content, cookie_search):
+    Phantom.debug("find_cookie")
     # https://stackoverflow.com/questions/69170069/python-requests-get-viewstate-and-eventvalidation-after-dopostback
     # Parse the webpage
-    soup = BeautifulSoup(HTMLContent, 'html.parser')
+    soup = BeautifulSoup(html_content, 'html.parser')
     # Find all input tags
     input_tags = soup.find_all('input')
-    logging.info("input_tags: "+str(input_tags))
+    Phantom.debug("input_tags: " + str(input_tags))
     # Find the  input field and get its value
-    global CookieInputValue
-    CookieInputValue = soup.find('input', {'id': CookieSearch}).get('value')
-    pprint_ForRealz(CookieSearch,CookieInputValue)
+    cookie_input_value = soup.find('input', {'id': cookie_search}).get('value')
+    pprint_simp(cookie_search, cookie_input_value)
+ 
+ 
+def az_auth():
+    Phantom.debug("az_auth")
+    r = requests.post('https://login.microsoftonline.com/' + tenant_id + '/oauth2/v2.0/token',
+                      data={'grant_type': 'client_credentials', 'client_id': app_id, 'client_secret': app_id_secret,
+                            'scope': 'https://graph.microsoft.com/.default'}, verify=SSL_Verify)
+    if r.status_code in [200]:
+        tok_dict = json.loads(r.text)
+        return tok_dict["access_token"]
+    else:
+        Phantom.error(r.status_code)
+        Phantom.error(r.url)
+        Phantom.error(r.text)
+        Phantom.error(r.content)
+        Phantom.error("ERROR DID NOT GET 200 STATUS CODE")
 
-# main-sh
 
-PROXY()
+def az_get(user, folder, azfilter):
+    Phantom.debug("az_get")
+    headers = {"Authorization": f"Bearer {access_token}", "Content-Type": "application/json"}
+    r = requests.get("https://graph.microsoft.com/beta/users/" + user + "/mailFolders/" + folder + "/messages" + azfilter,
+                     headers=headers, verify=SSL_Verify)
+    if r.status_code in [200]:
+        # Phantom.debug(r.status_code)
+        # Phantom.debug(r.url)
+        # pprint_simp('r.json()', r.json())
+        for i in range(len(r.json()['value'])):
+            print(r.json()['value'][i])
+        # Phantom.debug("DEBUG: Status 200 OK ")
+    else:
+        Phantom.error(r.status_code)
+        Phantom.error(r.url)
+        Phantom.error(r.text)
+        Phantom.error(r.content)
+        Phantom.error("ERROR DID NOT GET 200 STATUS CODE")
 
-url = 'https://akamai.indoorfinders.com/login.aspx'
-response1 = requests.get(url,verify=VAR_SSL_FLAG)
 
-FindCookie(response1.content,'__VIEWSTATE')
-FindCookie(response1.content,'__VIEWSTATEGENERATOR')
-FindCookie(response1.content,'__EVENTVALIDATION')
-FindCookie(response1.content,'ssoLinkButton')
+"""
+MAIN
+"""
+
+# load the config
+config = load_config('config.yml')
+app_id = config['app_id']
+app_id_secret = config['app_id_secret']
+tenant_id = config['tenant_id']
+
+# This is set to False when we enable proxy support later
+SSL_Verify = True
+
+# proxy_enable()
+
+access_token = az_auth()
+
+az_get('phishing@XXXXXXXXXXXXXX.com', 'SentItems', '?$select=webLink')
+
+"""
+TODO:
+* pageanation
+* get URL
+* get header info
+    _dns
+    _dimkeys
+    _spf
+    _whatever else in headers is juicy
 
 
+# /mailFolders/SentItems/messages?$count=true&$filter=isread%20eq%20false
+# /mailFolders/SentItems/messages?$select=sender,subject
+# Inbox, Drafts, SentItems, or DeletedItems
+
+# The Graph API URL for accessing the specific email and its attachments
+#$emailUrl = "https://graph.microsoft.com/v1.0/users/$userEmailOrId/messages/$messageId"
+#$attachmentsUrl = "$emailUrl/attachments"
+
+# https://learn.microsoft.com/en-us/archive/blogs/exchangedev/building-daemon-or-service-apps-with-office-365-mail-calendar-and-contacts-apis-oauth2-client-credential-flow
+# https://stackoverflow.com/questions/40763049/obtain-access-token-for-both-microsoft-graph-and-individual-service-api-endpoint
+# https://learn.microsoft.com/en-us/graph/api/message-get?view=graph-rest-1.0&tabs=http
+# mail.read docs
+# https://kagi.com/search?q=%22graph.microsoft.com%22+%22mail.read%22
+# https://learn.microsoft.com/en-us/archive/blogs/exchangedev/building-daemon-or-service-apps-with-office-365-mail-calendar-and-contacts-apis-oauth2-client-credential-flow
+# https://stackoverflow.com/questions/40763049/obtain-access-token-for-both-microsoft-graph-and-individual-service-api-endpoint
+# https://learn.microsoft.com/en-us/graph/api/message-get?view=graph-rest-1.0&tabs=http
 
 
-
-
-#url = 'https://newellbrands.indoorfinders.com/login.aspx'
-#response2 = requests.get(url,verify=VAR_SSL_FLAG,cookies=response1.cookies)
-
-#pprint_ForRealz(response2.cookies)
+"""
